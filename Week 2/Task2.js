a0fad5c20703c60594b0732b6d8c46f86e0de24f
Ojaswini Pradhan <ojaswinipradhan9@gmail.com>
2025-04-13 19:41:02 +0530

    Update Task2.js

--- Week 2/Task2.js
+++ Week 2/Task2.js
@@ -1,1 +1,12 @@
 
+let x = 10;
+let y = 5;
+
+let additionResult = x + y;
+let multiplicationResult = x * y;
+let modulusResult = x % y;
+
+console.log("Addition Result:", additionResult);
+console.log("Multiplication Result:", multiplicationResult);
+console.log("Modulus Result:", modulusResult);
+
