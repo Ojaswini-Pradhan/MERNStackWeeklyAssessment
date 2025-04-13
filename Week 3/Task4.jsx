c59fe1b12651ca6c17d7734b12af459be4d4ec86
Ojaswini Pradhan <ojaswinipradhan9@gmail.com>
2025-04-13 20:06:44 +0530

    Update Task4.jsx

--- Week 3/Task4.jsx
+++ Week 3/Task4.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-
-const ButtonComponent = () => {
-  const handleClick = () => {
-    console.log('Button clicked!');
-  };
-
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+const Button = ({ text }) => {
+    const handleClick = () => {
+      console.log("Button clicked!");
+    };
+  
+    return (
       <button
         onClick={handleClick}
-        className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition"
+        className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition"
       >
-        Click Me
+        {text}
       </button>
-    </div>
-  );
-};
-
-export default ButtonComponent;
+    );
+  };
+  
+  const App = () => {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+        <Button text="Click Me" />
+      </div>
+    );
+  };
+  
+  export default App;
+  
